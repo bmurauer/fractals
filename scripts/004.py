0d2bf8adcef8f986f3244628e8be432d85875b4a
Benjamin Murauer <b.murauer@posteo.de>
2021-11-15 19:42:57 +0100

    added script for flame 004

---
+++ scripts/004.py
@@ -0,0 +1,13 @@
+from fractals.flame import Flame, Transform
+from fractals.utils import get_flame_from_file
+
+xml = get_flame_from_file("flames/flames.flame", "004", None)
+flame = Flame.from_element(xml)
+flame.xforms[2].coefs.fully_rotate_linear(2)
+flame.xforms[3].coefs.fully_rotate_linear(3)
+flame.xforms[0].coefs.orbit_transform(radius=0.4)
+flame.palette.n_rotations = 1
+flames = flame.animate(n_frames=1000)
+flames.write_file()
+flames.render()
+flames.convert_to_movie()
