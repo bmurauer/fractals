8e5ecc0161ea4c510ee7b140603434c5dde7aae4
Benjamin Murauer <b.murauer@posteo.de>
2021-11-21 19:28:23 +0100

    added script for 027

---
+++ scripts/027.py
@@ -0,0 +1,14 @@
+from fractals.flame import Flame
+from fractals.utils import get_flame_from_file
+
+xml = get_flame_from_file("flames.flame", "027")
+flame = Flame.from_element(xml)
+flame.add_palette_rotation_animation()
+flame.xforms[0].add_scale_animation(factor=1.5, n_repetitions=2)
+flame.xforms[0].add_rotation_animation()
+flame.xforms[1].add_orbit_animation(radius=0.3, n_repetitions=3)
+flame.xforms[3].add_orbit_animation(radius=0.3, n_repetitions=3)
+flames = flame.animate(total_frames=1000)
+flames.write_file()
+flames.render()
+flames.convert_to_movie()
