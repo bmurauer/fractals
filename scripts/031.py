b5483324916c4609b49d56f38a08e85896947a24
Benjamin Murauer <b.murauer@posteo.de>
2021-11-25 21:43:15 +0100

    added script for 031

--- scripts/031.py
+++ scripts/031.py
@@ -1,12 +1,9 @@
 from fractals.flame import Flame
 from fractals.utils import get_flame_from_file
 
-xml = get_flame_from_file("flames.flame", "023")
+xml = get_flame_from_file("flames.flame", "031")
 flame = Flame.from_element(xml)
-flame.xforms[1].add_rotation_animation(1)
-flame.xforms[2].add_rotation_animation(2)
-flame.xforms[5].add_orbit_animation(radius=0.002)
-flame.palette.n_rotations = 1
+flame.xforms[0].add_attr_animation("elliptic", 1.1)
 flames = flame.animate(total_frames=1500)
 flames.write_file()
 flames.render()
